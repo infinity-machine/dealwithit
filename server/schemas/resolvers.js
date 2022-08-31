0a3a754a050168cf93e2e5a8653f65fe8f7e2d08
Connor Carciofini <ccarciofini@gmail.com>
2022-08-31 13:56:11 -0500

    began backend set up

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { User } = require('../models');
+const { signToken } = require('../auth');
+const { ApolloError } = require('apollo-server-express');
+
+const resolvers = {
+  // Query: {
+  //   async getTodos(_, args, context) {
+  //     if (!context.user) throw new ApolloError('not authorized', 402);
+
+  //     return await Todo.find();
+  //   }
+  // },
+
+  Mutation: {
+    async addUser(_, { email, password }, context) {
+      try {
+        const user = await User.create({ email, password });
+
+        const token = signToken(user);
+        return { user, token };
+
+      } catch (err) {
+        throw new ApolloError(err);
+      }
+    },
+
+    async loginUser(_, { email, password }, context) {
+      const user = await User.findOne({ email });
+
+      if (!user) throw new ApolloError('No user found with that email address');
+
+      if (!user.validatePass(password)) throw new ApolloError('Your password is incorrect');
+
+      try {
+        const token = signToken(user);
+
+        return { user, token };
+      } catch (err) {
+        throw new ApolloError(err);
+      }
+    },
+  }
+};
+
+module.exports = resolvers;
