78742470ce0da4ec98d767b13908daa6bdf7f3f6
Connor Carciofini <ccarciofini@gmail.com>
2022-09-01 11:18:50 -0500

    worked on user model

--- server/models/User.js
+++ server/models/User.js
@@ -8,9 +8,16 @@ const userSchema = new Schema({
     unique: true,
     match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i
   },
+  username: {
+    type: String,
+    required: true
+  },
   password: {
     type: String,
     required: true
+  },
+  bank: {
+    type: Number
   }
 });
 
