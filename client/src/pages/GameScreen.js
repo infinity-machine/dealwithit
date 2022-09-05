7f569a3b4b2592919ff068b3f83c20a0faabb65e
Steve Lang <langloiss0003@my.uwstout.edu>
2022-09-05 18:36:49 -0500

    bets working

--- client/src/pages/GameScreen.js
+++ client/src/pages/GameScreen.js
@@ -1,10 +1,29 @@
 import { useEffect, useState } from "react"
 import UserBet from '../components/UserBet'
+import { useMutation, gql } from '@apollo/client'
+
+const ADD_BET = gql`
+    mutation addBet($user_bet: Int!, $winner: String!) {
+        addBet(user_bet: $user_bet, winner: $winner){
+            user {
+            _id
+            email
+            username
+            bank
+            }
+            token
+        }
+    }    `
+// 1000 - 500 = 500
 
 function GameScreen(props) {
-    const [playercard, setPlayerCard] = useState(null)
-    const [compcard, setCompCard] = useState(null)
-    const [winner, setWinnerCard] = useState('')
+    const [playercard, setPlayerCard] = useState(null);
+    const [compcard, setCompCard] = useState(null);
+    const [winner, setWinnerCard] = useState('');
+    const [betAmount, setBetAmount] = useState(0);
+
+    const [addBet, { loading, error, data }] = useMutation(ADD_BET);
+
     // SHUFFLE NEW DECK OR DECKS
     // WE WILL HAVE TO GET DECK ID FROM THIS FETCH 
 
@@ -17,7 +36,7 @@ function GameScreen(props) {
     // PLUG IN DECK ID FROM FIRST FETCH
     // YOU CAN ALSO DRAW FROM 'new' DECK
     // YOU CAN SPECIFY HOW MANY CARDS WITH THE COUNT=X QUERY
-    const grabDeck = () => {
+    const grabDeck = async () => {
         fetch(`https://www.deckofcardsapi.com/api/deck/new/draw/?count=2`)
             .then((res) => res.json())
             .then(data => {
@@ -38,7 +57,7 @@ function GameScreen(props) {
                         case 'JACK':
                             card = 11
                             break
-                            default:
+                        default:
                     }
                     card = parseInt(card)
                     console.log(card)
@@ -48,20 +67,29 @@ function GameScreen(props) {
                 let playercard = convertCard(data.cards[0].value)
                 let compcard = convertCard(data.cards[1].value)
 
+                // Call server to update based on bet amt and win/loss
+                const userWon = playercard > compcard;
+                addBet({ variables: { user_bet: betAmount, winner: userWon ? 'player' : 'comp' } }).then((response) => {
+                    props.setUser(response.data.addBet.user);
+                })
 
                 if (playercard > compcard) {
                     setWinnerCard('player')
                 } else setWinnerCard('comp')
 
             })
+
     }
 
+    const setBetCb = (betNumber) => {
+        setBetAmount(betNumber);
+    }
 
-    // useEffect(grabDeck, [])
+    useEffect(grabDeck, [])
 
     return (
         <div>
-            < UserBet winner={winner}user={props.user} setUser={props.setUser}/>
+            < UserBet winner={winner} user={props.user} setUser={props.setUser} betAmount={betAmount} setBetCb={setBetCb} />
             <h1>GameScreen</h1>
             {playercard && (
                 <div className="cards">
@@ -71,7 +99,7 @@ function GameScreen(props) {
             )}
             {winner && (winner === 'player' ? <h1>Player Wins!</h1> : <h1>Computer Wins!</h1>)}
             <div>
-                <button onClick={grabDeck}>Deal</button>
+                <button onClick={grabDeck} disabled={isNaN(betAmount)}>Deal</button>
             </div>
         </div>
     )
